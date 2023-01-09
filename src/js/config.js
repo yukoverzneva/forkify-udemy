853b2d0d588e333435995802fca63994db4d8358
yukoverzneva <yukoverzneva@gmail.com>
2023-01-09 15:49:53 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = '88b2e510-a54b-4a2f-874d-4fac629eb24c';
+export const MODAL_CLOSE_SEC = 2.5;
